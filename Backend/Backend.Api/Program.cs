285b98fce108003a2de4b553f75f5de78ccd95dc
Kim Andersson <Kimpabooy@live.com>
2026-04-14 16:12:05 +0200

    Restructure solution

--- Backend/Backend.Api/Program.cs
+++ Backend/Backend.Api/Program.cs
@@ -1,10 +1,11 @@
-
-namespace Backend
+namespace Backend.WebApi
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("hello");
+            Console.ReadKey();
         }
     }
 }

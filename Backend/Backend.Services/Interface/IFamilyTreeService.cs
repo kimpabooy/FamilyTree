e8da48e42d0ca6c0003ed3cf563072344e3545b8
Kim Andersson <Kimpabooy@live.com>
2026-05-11 13:33:30 +0200

    Add async CRUD methods to FamilyTreeService and interface
    more of a placeholder to test endpoints

--- Backend/Backend.Services/Interface/IFamilyTreeService.cs
+++ Backend/Backend.Services/Interface/IFamilyTreeService.cs
@@ -1,6 +1,13 @@
-﻿namespace Backend.Services.Interface
+﻿using Backend.Core.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Backend.Services.Interface
 {
     public interface IFamilyTreeService
     {
+        Task<FamilyTree?> GetFamilyTreeAsync(CancellationToken cancellationToken = default);
+        Task<FamilyTree?> GetFamilyTreeByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<FamilyTree?> CreateFamilyTreeAsync(FamilyTree familyTree, CancellationToken cancellationToken = default);
     }
 }
--- Backend/Backend.Services/Services/FamilyTreeService.cs
+++ Backend/Backend.Services/Services/FamilyTreeService.cs
@@ -1,5 +1,9 @@
 ﻿using Backend.Core.Interface;
+using Backend.Core.Models;
 using Backend.Services.Interface;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Backend.Services.Services
 {
@@ -10,6 +14,31 @@ namespace Backend.Services.Services
         public FamilyTreeService(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+        }
+
+        public async Task<FamilyTree?> GetFamilyTreeAsync(CancellationToken cancellationToken = default)
+        {
+            var list = await _unitOfWork.FamilyTreeRepository.GetAllAsync(cancellationToken);
+            return list.FirstOrDefault();
+        }
+
+        public async Task<FamilyTree?> GetFamilyTreeByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _unitOfWork.FamilyTreeRepository.GetByIdAsync(id, cancellationToken);
+        }
+
+        public async Task<FamilyTree?> CreateFamilyTreeAsync(FamilyTree familyTree, CancellationToken cancellationToken = default)
+        {
+            var treeToCreate = new FamilyTree
+            {
+                Name = familyTree.Name,
+                IsPublic = familyTree.IsPublic,
+                OwnerId = familyTree.OwnerId
+            };
+
+            var createdFamilyTree = _unitOfWork.FamilyTreeRepository.Add(treeToCreate);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+            return createdFamilyTree;
         }
     }
 }

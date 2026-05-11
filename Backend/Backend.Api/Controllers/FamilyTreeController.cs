2e67ab357ea2ceb68129cc5840e3e9a6fb576501
Kim Andersson <Kimpabooy@live.com>
2026-05-11 13:29:36 +0200

    Add FamilyTreeController with basic GET endpoints
    Introduced FamilyTreeController to expose API endpoints for retrieving family tree data. Includes GET endpoints for fetching all family trees and by ID, with dependency injection of IFamilyTreeService. Added a placeholder for a future POST endpoint with a note to implement validation and error handling.

---
+++ Backend/Backend.Api/Controllers/FamilyTreeController.cs
@@ -0,0 +1,47 @@
+﻿using Backend.Core.Models;
+using Backend.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FamilyTreeController : ControllerBase
+    {
+        private readonly IFamilyTreeService _familyTreeService;
+
+        public FamilyTreeController(IFamilyTreeService familyTreeService)
+        {
+            _familyTreeService = familyTreeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFamilyTree(CancellationToken cancellationToken)
+        {
+            var familyTree = await _familyTreeService.GetFamilyTreeAsync(cancellationToken);
+            if (familyTree is null) return NotFound();
+
+            return Ok(familyTree);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id, CancellationToken cancellationToken)
+        {
+            var tree = await _familyTreeService.GetFamilyTreeByIdAsync(id, cancellationToken);
+            if (tree is null) return NotFound();
+            
+            return Ok(tree);
+        }
+
+        //[HttpPost]
+        // TODO: fix POST and Add validation and error handling
+
+        //public async Task<IActionResult> CreateFamilyTree(FamilyTree familyTree, CancellationToken cancellationToken)
+        //{
+        //    var createdFamilyTree = await _familyTreeService.CreateFamilyTreeAsync(familyTree, cancellationToken);
+        //    if (createdFamilyTree is null) return BadRequest();
+
+        //    return CreatedAtAction(nameof(GetById), new { id = createdFamilyTree.Id }, createdFamilyTree);
+        //}
+    }
+}

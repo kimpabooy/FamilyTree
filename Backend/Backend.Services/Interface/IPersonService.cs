9b0f4f58d9b16926198ffa70e0680309e086cdb6
Kim Andersson <Kimpabooy@live.com>
2026-05-18 12:22:30 +0200

    Added service interface for Person

--- Backend/Backend.Services/Interface/IPersonService.cs
+++ Backend/Backend.Services/Interface/IPersonService.cs
@@ -1,6 +1,13 @@
-﻿namespace Backend.Services.Interface
+﻿using Backend.Core.Models;
+
+namespace Backend.Services.Interface
 {
     public interface IPersonService
     {
+        Task<IEnumerable<Person>> GetAllByFamilyTreeIdAsync(int familyTreeId, CancellationToken cancellationToken = default);
+        Task<Person?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<Person?> CreateAsync(Person person, CancellationToken cancellationToken = default);
+        Task<Person?> UpdateAsync(int id, Person person, CancellationToken cancellationToken = default);
+        Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default);
     }
 }

6b16cabfcb5efb05751dabd052a43e940a789dcf
Kim Andersson <Kimpabooy@live.com>
2026-05-18 12:34:08 +0200

    Added service interface for ParentChildRelation

--- Backend/Backend.Services/Interface/IParentChildRelationService.cs
+++ Backend/Backend.Services/Interface/IParentChildRelationService.cs
@@ -1,6 +1,14 @@
-﻿namespace Backend.Services.Interface
+﻿using Backend.Core.Models;
+
+namespace Backend.Services.Interface
 {
     public interface IParentChildRelationService
     {
+        Task<ParentChildRelation?> CreateAsync(int parentId, int childId, CancellationToken cancellationToken = default);
+        Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Person>> GetParentsAsync(int personId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Person>> GetChildrenAsync(int personId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Person>> GetSiblingsAsync(int personId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Person>> GetGrandparentsAsync(int personId, CancellationToken cancellationToken = default);
     }
-}
+}   

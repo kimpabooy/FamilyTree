50025e3fc114c3fa6b56f808d6ea9452a760bfd8
Kim Andersson <Kimpabooy@live.com>
2026-05-19 22:19:24 +0200

    Add ResponsePersonFamily class to DTOs namespace

---
+++ Backend/Backend.Services/DTOs/Person/ResponsePersonFamily.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Services.DTOs.Relations;
+
+namespace Backend.Services.DTOs.Person
+{
+    public class ResponsePersonFamily
+    {
+        public ResponsePerson? Person { get; set; }
+        public IEnumerable<ResponsePerson>? Parents { get; set; }
+        public IEnumerable<ResponsePerson>? Children { get; set; }
+        public IEnumerable<ResponsePerson>? Siblings { get; set; }
+        public IEnumerable<ResponsePerson>? Grandparents { get; set; }
+        public IEnumerable<ResponsePartnerRelation>? Partners { get; set; }
+    }
+}

aedcbfe5904ca86fcf8d8ba71d9428b6cb043ec4
Kim Andersson <Kimpabooy@live.com>
2026-05-19 16:01:47 +0200

    Added some DTO's for Person

---
+++ Backend/Backend.Services/DTOs/Person/RequestUpdatePerson.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Core.Models;
+
+namespace Backend.Services.DTOs.Person
+{
+    public class RequestUpdatePerson
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? DeathDate { get; set; }
+        public Gender Gender { get; set; } = Gender.Other; // Default to Other if not specified
+        public string? ProfileImageUrl { get; set; }
+        public int? FamilyTreeId { get; set; }
+    }
+}

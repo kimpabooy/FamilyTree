9833e2a7621ee14a8ac5cbbfde446948d4ac8cba
Kim Andersson <Kimpabooy@live.com>
2026-05-20 14:38:24 +0200

    Add DTOs for authentication requests and responses
    Introduced a new namespace `Backend.Services.DTOs.Auth` to
    organize Data Transfer Objects (DTOs) for authentication.
    
    Added the `RequestLogin` class with `Email` and `Password`
    properties for handling login requests.
    
    Added the `RequestRegister` class with `DisplayName`, `Email`,
    and `Password` properties for handling user registration.
    
    Added the `ResponseAuth` class with `Token`, `UserId`, `Email`,
    and `DisplayName` properties to structure authentication
    response data.

---
+++ Backend/Backend.Services/DTOs/Auth/RequestRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Services.DTOs.Auth
+{
+    public class RequestRegister
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

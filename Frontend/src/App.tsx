a3c640b037da9ea22cbad7d2cc535b7bac52cc90
Kim Andersson <Kimpabooy@live.com>
2026-05-23 13:54:10 +0200

    Set layout height to 100vh and adjust main content overflow for improved scrolling behavior

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import BasicFlow from './components/Flow/BasicFlow'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import SidePanel from './components/sidepanel'
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <SidePanel />
         <main className="main-content">
+          <BasicFlow />
         </main>
         <Footer />
       </div>
